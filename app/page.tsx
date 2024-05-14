8fb9292885bb4a9f89b846251456cc630dd80f68
Jinay Patel <pateljinay@outlook.com>
2024-05-14 18:32:57 +0000

    fixed the grid issue and I'm currently working on the dialogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,71 +1,52 @@
 import * as React from "react";
 
+import { Card } from "@/components/ui/card";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 interface HardwareCardProps {
 	rowSpan: number;
 	colSpan: number;
 	name: string;
+	description: string;
 }
 
-function HardwareCard({ rowSpan, colSpan, name }: HardwareCardProps) {
+function HardwareCard({
+	rowSpan,
+	colSpan,
+	name,
+	description,
+}: HardwareCardProps) {
 	return (
-		<Card
-			className={`w-full h-full col-span-${colSpan} row-span-${rowSpan} flex items-center justify-center`}
-		>
-			<h2 className="text-center text-3xl font-semibold tracking-tight py-24 px-2">
-				{name}
-			</h2>
-		</Card>
+		<Dialog>
+			<DialogTrigger asChild>
+				<Card
+					className={`w-full h-full col-span-${colSpan} row-span-${rowSpan} flex items-center justify-center hover:cursor-pointer`}
+				>
+					<h2 className="text-center text-3xl font-semibold tracking-tight py-24 px-2">
+						{name}
+					</h2>
+				</Card>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>{name}</DialogTitle>
+					<DialogDescription>{description}</DialogDescription>
+				</DialogHeader>
+				<div className="grid gap-4 py-4"></div>
+			</DialogContent>
+		</Dialog>
 	);
 }
 
 export default function Home() {
-	const cardParameters: Array<HardwareCardProps> = [
-		{
-			rowSpan: 4,
-			colSpan: 3,
-			name: "CPU",
-		},
-		{
-			rowSpan: 5,
-			colSpan: 1,
-			name: "Heat Sink",
-		},
-		{
-			rowSpan: 2,
-			colSpan: 3,
-			name: "Hard Drive / SSD",
-		},
-		{
-			rowSpan: 2,
-			colSpan: 3,
-			name: "RAM",
-		},
-		{
-			rowSpan: 1,
-			colSpan: 3,
-			name: "Power Supply / Battery",
-		},
-		{
-			rowSpan: 1,
-			colSpan: 3,
-			name: "Expansion Slots",
-		},
-		{
-			rowSpan: 1,
-			colSpan: 7,
-			name: "GPU",
-		},
-	];
-
 	return (
 		<div className="p-10">
 			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
@@ -73,23 +54,53 @@ export default function Home() {
 			</h1>
 			<div
 				className="border rounded-lg bg-card text-card-foreground shadow w-[80rem] gap-8 p-8 grid grid-cols-7
-		mx-auto mt-16"
+			 auto-rows-max mx-auto mt-16"
 			>
 				<h2 className="text-4xl col-span-7 text-center font-semibold tracking-tight m-0 p-0">
 					Mother Board
 				</h2>
-				{cardParameters.map((object, index) => {
-					return (
-						<Card
-							className={`w-full h-full col-span-${object.colSpan} row-span-${object.rowSpan} flex items-center justify-center`}
-							key={index}
-						>
-							<h2 className="text-center text-3xl font-semibold tracking-tight py-24 px-2">
-								{object.name}
-							</h2>
-						</Card>
-					);
-				})}
+				<HardwareCard
+					rowSpan={4}
+					colSpan={3}
+					name="CPU"
+					description="aldsjflkasdjfkl;"
+				/>
+				<HardwareCard
+					rowSpan={5}
+					colSpan={1}
+					name="Heat Sink"
+					description="aldsjflkasdjfkl;"
+				/>
+				<HardwareCard
+					rowSpan={2}
+					colSpan={3}
+					name="Hard Drive / SSD"
+					description="aldsjflkasdjfkl;"
+				/>
+				<HardwareCard
+					rowSpan={2}
+					colSpan={3}
+					name="RAM"
+					description="aldsjflkasdjfkl;"
+				/>
+				<HardwareCard
+					rowSpan={1}
+					colSpan={3}
+					name="Power Supply / Battery"
+					description="aldsjflkasdjfkl;"
+				/>
+				<HardwareCard
+					rowSpan={1}
+					colSpan={3}
+					name="Expansion Slots"
+					description="aldsjflkasdjfkl;"
+				/>
+				<HardwareCard
+					rowSpan={1}
+					colSpan={7}
+					name="GPU"
+					description="aldsjflkasdjfkl;"
+				/>
 			</div>
 		</div>
 	);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,80 +1,102 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
 	],
-  prefix: "",
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
-  },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+	prefix: "",
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		extend: {
+			colors: {
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
+	safelist: [
+		"col-span-1",
+		"col-span-2",
+		"col-span-3",
+		"col-span-4",
+		"col-span-5",
+		"col-span-6",
+		"col-span-7",
+		"col-span-8",
+		"col-span-9",
+		"col-span-10",
+		"row-span-1",
+		"row-span-2",
+		"row-span-3",
+		"row-span-4",
+		"row-span-5",
+		"row-span-6",
+		"row-span-7",
+		"row-span-8",
+		"row-span-9",
+		"row-span-10",
+	],
+} satisfies Config;
 
-export default config
+export default config;
